c0f0a2ca09bf64d9bf200fb2f2a14cea45011dcd
Aly Norrgard <amnorrgard@gmail.com>
2019-06-30 13:20:28 -0400

    added approach for Single Riffle Shuffle

---
+++ singleRiffleShuffle.js
@@ -0,0 +1,9 @@
+// APPROACH
+// - initiate pointers for half1 and half2 to start at 0
+// - loop over shuffledDeck
+//      - if shuffledDeck[i] === half1[pointer1], then increment pointer1 by 1
+//      - else if shuffledDeck[i] === half2[pointer2], then increment pointer2 by 1
+//      - else return false
+// - if loop completes, we know shuffledDeck is a single riffle of half1 and half2, so we can return true
+
+function isSingleRiffle(half1, half2, shuffledDeck) {}
