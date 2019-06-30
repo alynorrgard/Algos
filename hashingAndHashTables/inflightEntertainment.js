adc89cad274eee34435cdddec7e3aa8b4e267953
Aly Norrgard <amnorrgard@gmail.com>
2019-06-30 15:03:20 -0400

    completed function, passes all tests

--- hashingAndHashTables/inflightEntertainment.js
+++ hashingAndHashTables/inflightEntertainment.js
@@ -6,3 +6,18 @@
 //          - movie2Length = movieLengths[j]
 //          - if movie1Length + movie2Length = flightLength return true
 // - return false
+
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+  for (let i = 0; i < movieLengths.length - 1; i++) {
+    const movie1Length = movieLengths[i];
+
+    for (let j = 1; j < movieLengths.length; j++) {
+      const movie2Length = movieLengths[j];
+
+      if (movie1Length + movie2Length === flightLength) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
