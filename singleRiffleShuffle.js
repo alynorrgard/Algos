69c0ff3bd8ab9b0441609bf2b83e5f487cd01875
Aly Norrgard <amnorrgard@gmail.com>
2019-06-30 13:25:34 -0400

    completed function, passes all tests

--- singleRiffleShuffle.js
+++ singleRiffleShuffle.js
@@ -6,4 +6,19 @@
 //      - else return false
 // - if loop completes, we know shuffledDeck is a single riffle of half1 and half2, so we can return true
 
-function isSingleRiffle(half1, half2, shuffledDeck) {}
+function isSingleRiffle(half1, half2, shuffledDeck) {
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  for (let i = 0; i < shuffledDeck.length; i++) {
+    if (shuffledDeck[i] === half1[pointer1]) {
+      pointer1++;
+    } else if (shuffledDeck[i] === half2[pointer2]) {
+      pointer2++;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
