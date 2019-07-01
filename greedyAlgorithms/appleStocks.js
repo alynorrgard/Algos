75aa7ad633a62db2da670217a183f055cc3d6195
Aly Norrgard <amnorrgard@gmail.com>
2019-06-30 20:48:44 -0400

    added solution for Apple Stocks, passes all tests

--- greedyAlgorithms/appleStocks.js
+++ greedyAlgorithms/appleStocks.js
@@ -6,3 +6,21 @@
 //          - initialize pointer for sellPrice
 //          - if profit (sellPrice - buyPrice) > highestProfit, reassign highestProfit
 // - return highestProfit
+
+function getMaxProfit(stockPrices) {
+  if (stockPrices.length < 2) {
+    throw new Error();
+  }
+
+  let minPrice = stockPrices[0];
+  let highestProfit = stockPrices[1] - stockPrices[0];
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    const currentPrice = stockPrices[i];
+    const profit = currentPrice - minPrice;
+
+    highestProfit = Math.max(highestProfit, profit);
+    minPrice = Math.min(minPrice, currentPrice);
+  }
+  return highestProfit;
+}
